e6d8e7b089af259318207d8186c38d2440e74970
Juan David Grisales Garzon <juandavid.grisales@gmail.com>
2016-09-22 16:25:24 -0500

    firts commit

---
+++ src/main/java/cool/pidelo/apps/cubesummation/CubeSummationBusiness.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cool.pidelo.apps.cubesummation;
+
+/**
+ *
+ * @author juan.grisales
+ */
+public class CubeSummationBusiness {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
