ffb388b28e5b1ae0a05442699dbd35870fec5094
Juan David Grisales Garzon <juandavid.grisales@gmail.com>
2016-09-23 08:47:27 -0500

    Cube Summation / Juan Grisales - add layers and empty classes to project

---
+++ src/main/java/cool/pidelo/apps/cubesummation/api/CubeSummationAPI.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cool.pidelo.apps.cubesummation.api;
+
+/**
+ *
+ * @author juan.grisales
+ */
+public class CubeSummationAPI {
+    
+}
--- src/main/java/cool/pidelo/apps/cubesummation/service/CubeSummationBusiness.java
+++ src/main/java/cool/pidelo/apps/cubesummation/service/CubeSummationBusiness.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cool.pidelo.apps.cubesummation;
+package cool.pidelo.apps.cubesummation.service;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
